39a90d19f823a08fc8e57722ccc4e76f003be41a
Mateus Vieira <mateus_vieira1@hotmail.com>
2025-06-15 21:15:35 -0300

    Feito até a aula 5.4

---
+++ main.js
@@ -0,0 +1,23 @@
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
