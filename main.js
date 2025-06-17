79ec42c8e6c211f7a3fee738f45da8867d91968a
Mateus Vieira <mateus_vieira1@hotmail.com>
2025-06-16 21:21:23 -0300

    Versão 1.0 - Finalizado conforme o curso

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento  = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
@@ -13,11 +20,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     tecla.onclick = function () {
         tocaSom(idAudio)
     }
-    // tecla.onkeydown = function () {
-    //     tecla.classList.add('ativa');
-    // }
-    // tecla.onkeyup = function () {
-    //     tecla.classList.remove('ativa');
-    // }
+    tecla.onkeydown = function (event) {
+        console.log(event.code)
+        if (event.code === 'Space' || event.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 
 }
